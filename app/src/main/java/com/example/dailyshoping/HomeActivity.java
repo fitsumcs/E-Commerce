63f94edf297477a5e66e2f762d1c777bb7e517fa
Fitsum Alemu <fitsumcs@gmail.com>
2020-11-17 16:54:58 +0300

    Add cls

--- app/src/main/java/com/example/dailyshoping/HomeActivity.java
+++ app/src/main/java/com/example/dailyshoping/HomeActivity.java
@@ -6,18 +6,23 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.Dialog;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.text.DateFormat;
+import java.util.Date;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -80,6 +85,35 @@ public class HomeActivity extends AppCompatActivity {
         final EditText amount=dialog .findViewById(R.id.edt_ammount);
         final EditText note=dialog .findViewById(R.id.edt_note);
         Button btnSave=dialog .findViewById(R.id.btn_save);
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String mType=type.getText().toString().trim();
+                String mAmount=amount.getText().toString().trim();
+                String mNote=note.getText().toString().trim();
+
+                int ammint=Integer.parseInt(mAmount);
+
+                if (TextUtils.isEmpty(mType) || TextUtils.isEmpty(mAmount) || TextUtils.isEmpty(mNote)){
+                    type.setError("All Fields are  Required..");
+                    return;
+                }
+
+                String id= mDatabase.push().getKey();
+
+                String date= DateFormat.getDateInstance().format(new Date());
+
+                ShopingModle data = new ShopingModle(mType,ammint,mNote,date,id);
+
+                mDatabase.child(id).setValue(data);
+
+                Toast.makeText(getApplicationContext(),"Data Added Successfully !! ",Toast.LENGTH_SHORT).show();
+
+                dialog.dismiss();
+            }
+        });
 
 
     }
