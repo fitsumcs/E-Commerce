fbc524f2f7daaba4dd509e80d7a4c38992d97966
Fitsum Alemu <fitsumcs@gmail.com>
2020-11-17 16:13:53 +0300

    Adding firebase on the home page with elements initialized

--- app/src/main/java/com/example/dailyshoping/HomeActivity.java
+++ app/src/main/java/com/example/dailyshoping/HomeActivity.java
@@ -1,16 +1,62 @@
 package com.example.dailyshoping;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class HomeActivity extends AppCompatActivity {
 
 
 
+    private FloatingActionButton fab_btn;
+
+    private DatabaseReference mDatabase;
+    private FirebaseAuth mAuth;
+
+    private RecyclerView recyclerView;
+
+    private TextView totalsumResult;
+
+    //Global variable..
+
+    private String type;
+    private int amount;
+    private String note;
+    private String post_key;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        totalsumResult=findViewById(R.id.total_ammount);
+
+        mAuth=FirebaseAuth.getInstance();
+
+        FirebaseUser mUser=mAuth.getCurrentUser();
+        String uId=mUser.getUid();
+
+        mDatabase= FirebaseDatabase.getInstance().getReference().child("Shopping List").child(uId);
+
+        mDatabase.keepSynced(true);
+
+
+        recyclerView=findViewById(R.id.recycler_home);
+        LinearLayoutManager layoutManager=new LinearLayoutManager(this);
+
+        layoutManager.setStackFromEnd(true);
+        layoutManager.setReverseLayout(true);
+
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(layoutManager);
     }
 }
