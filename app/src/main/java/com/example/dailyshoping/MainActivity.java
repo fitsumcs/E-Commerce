0dfd4b81869f86726bdf2a6a92dbdfea6d8ffe66
Fitsum Alemu <fitsumcs@gmail.com>
2020-11-17 15:37:19 +0300

    user registeration on firebase done

--- app/src/main/java/com/example/dailyshoping/MainActivity.java
+++ app/src/main/java/com/example/dailyshoping/MainActivity.java
@@ -2,17 +2,33 @@ package com.example.dailyshoping;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
     }
 
     public void goToSignUp(View view) {
+        startActivity(new Intent(this,RegisterActivty.class));
+    }
+
+    public void loginUser(View view) {
     }
 }
--- app/src/main/java/com/example/dailyshoping/RegisterActivty.java
+++ app/src/main/java/com/example/dailyshoping/RegisterActivty.java
@@ -1,18 +1,80 @@
 package com.example.dailyshoping;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivty extends AppCompatActivity {
+
+    private EditText email;
+    private EditText pass;
+    private TextView signin;
+    private Button btnReg;
+
+    private FirebaseAuth mAuth;
+
+    private ProgressDialog mDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_activty);
+
+        mAuth= FirebaseAuth.getInstance();
+
+        mDialog=new ProgressDialog(this);
+
+        email=findViewById(R.id.email_reg);
+        pass=findViewById(R.id.password_reg);
+
+        btnReg=findViewById(R.id.btn_reg);
+        signin=findViewById(R.id.signin_txt);
+
     }
 
     public void goToSignIn(View view) {
+        startActivity(new Intent(this,MainActivity.class));
+    }
+
+    public void registerUser(View view) {
+        String mEmail=email.getText().toString().trim();
+        String mPass=pass.getText().toString().trim();
+
+        if (TextUtils.isEmpty(mEmail) || TextUtils.isEmpty(mPass)){
+            email.setError("All Fields are  Required ..");
+            return;
+        }
+
+        mDialog.setMessage("Processing..");
+        mDialog.show();
+        mAuth.createUserWithEmailAndPassword(mEmail,mPass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                mDialog.dismiss();
+                if (task.isSuccessful()){
+                    startActivity(new Intent(getApplicationContext(),HomeActivity.class));
+                    Toast.makeText(getApplicationContext(),"Successful",Toast.LENGTH_SHORT).show();
+
+                }else {
+                    Toast.makeText(getApplicationContext(),"Failed..",Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
     }
 }
