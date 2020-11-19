4067618dba9b2350a3fae409c30716822eda1db1
Fitsum Alemu <fitsumcs@gmail.com>
2020-11-19 14:07:42 +0300

    changing the date format

--- app/src/main/java/com/example/dailyshoping/UtilitiesClass.java
+++ app/src/main/java/com/example/dailyshoping/UtilitiesClass.java
@@ -3,8 +3,8 @@ package com.example.dailyshoping;
 import android.content.Context;
 import android.net.ConnectivityManager;
 
-import java.text.DateFormat;
-import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class UtilitiesClass {
     public boolean isNetworkAvailable(Context context) {
@@ -14,8 +14,10 @@ public class UtilitiesClass {
 
     public  String getFormatedDate()
     {
-        String date= DateFormat.getDateInstance().format(new Date());
-        return  date;
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("EEE, dd MMM yyyy");
+        Calendar calendar = Calendar.getInstance();
+        String today = simpleDateFormat.format(calendar.getTime());
+        return today;
     }
 
 }
