c81fa9c0ef77422a8e840cce09585252dc45e260
Fitsum Alemu <fitsumcs@gmail.com>
2020-11-19 19:15:22 +0300

    remove click listner from the record aactivity

--- app/src/main/java/com/example/dailyshoping/ShoppingRecord.java
+++ app/src/main/java/com/example/dailyshoping/ShoppingRecord.java
@@ -1,15 +1,8 @@
 package com.example.dailyshoping;
 
-import android.app.Dialog;
-import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.view.View;
-import android.view.Window;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -19,8 +12,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -31,9 +22,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
-import java.util.List;
 
 public class ShoppingRecord extends AppCompatActivity implements  OnItemClickedListener{
 
@@ -123,6 +112,10 @@ public class ShoppingRecord extends AppCompatActivity implements  OnItemClickedL
 
     }
 
+    public void onItemClicked(int position) {
+
+    }
+
     public void loadActivity()
     {
 
@@ -143,11 +136,6 @@ public class ShoppingRecord extends AppCompatActivity implements  OnItemClickedL
             recyclerView.setVisibility(View.GONE);
         }
     }
-
-    public void addShoppingItem(View view) {
-        addShoppingDialog();
-    }
-
 
 
     private void readAllData(String date) {
@@ -201,55 +189,6 @@ public class ShoppingRecord extends AppCompatActivity implements  OnItemClickedL
 
     }
 
-    private void addShoppingDialog() {
-        Dialog dialog = new Dialog(this);
-        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        dialog.setContentView(R.layout.add_shoping_item);
-        dialog.show();
-
-        final Spinner type= dialog .findViewById(R.id.edt_type);
-        final EditText amount=dialog .findViewById(R.id.edt_ammount);
-        final EditText note=dialog .findViewById(R.id.edt_note);
-        Button btnSave=dialog .findViewById(R.id.btn_save);
-
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.shoppingType_array, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        type.setAdapter(adapter);
-
-
-
-        btnSave.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                String mType=type.getSelectedItem().toString();
-                String mAmount=amount.getText().toString().trim();
-                String mNote=note.getText().toString().trim();
-
-
-
-                if (TextUtils.isEmpty(mType) || TextUtils.isEmpty(mAmount) || TextUtils.isEmpty(mNote)){
-                    Toast.makeText(getApplicationContext(),"All Fields are  Required.. ",Toast.LENGTH_SHORT).show();
-                    return;
-                }
-
-                float ammint = Float.parseFloat(mAmount);
-
-                String id= mDatabase.push().getKey();
-
-                String date= new UtilitiesClass().getFormatedDate();
-
-                ShopingModle data = new ShopingModle(mType,ammint,mNote,date,id);
-
-                writeToDb(id,data,"Data Added Successfully !! ");
-
-
-                dialog.dismiss();
-            }
-        });
-
-
-    }
 
     public  void loadTotalAmount(String date)
     {
@@ -285,119 +224,4 @@ public class ShoppingRecord extends AppCompatActivity implements  OnItemClickedL
         });
     }
 
-
-    //logout
-
-    public  void logout()
-    {
-        FirebaseAuth.getInstance().signOut();
-        startActivity(new Intent(getApplicationContext(),MainActivity.class));
-        finish();
-    }
-
-    @Override
-    public void onItemClicked(int position) {
-
-        ShopingModle shopingModle = shope_List.get(position);
-
-        String type=shopingModle.getType();
-        float amount=shopingModle.getAmount();
-        String note=shopingModle.getNote();
-        String myId = shopingModle.getId();
-        uppdateDialog(myId ,type,amount,note);
-
-
-
-    }
-
-    private void uppdateDialog( String id,String type, float amount, String note) {
-
-        Dialog dialog = new Dialog(this);
-        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        dialog.setContentView(R.layout.update_layout);
-        dialog.show();
-
-
-
-        final Spinner Vtype= dialog .findViewById(R.id.edt_typeUpdate);
-        final EditText Vamount =dialog .findViewById(R.id.edt_ammount_upd);
-        final EditText Vnote =dialog .findViewById(R.id.edt_note_upd);
-
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.shoppingType_array, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        Vtype.setAdapter(adapter);
-
-        List<String> Lines = Arrays.asList(getResources().getStringArray(R.array.shoppingType_array));
-
-        int spinnerPosition = Lines.indexOf(type);
-        Vtype.setSelection(spinnerPosition);
-        Vamount.setText(String.valueOf(amount));
-        Vnote.setText(note);
-
-
-
-
-        Button btnUpdate = dialog .findViewById(R.id.btn_SAVE_upd);
-        Button btnDelete = dialog .findViewById(R.id.btn_delete_upd);
-
-        btnUpdate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                String mType=Vtype.getSelectedItem().toString();
-                String mAmount=Vamount.getText().toString().trim();
-                String mNote=Vnote.getText().toString().trim();
-                float ammint = Float.parseFloat(mAmount);
-
-                if (TextUtils.isEmpty(mType) || TextUtils.isEmpty(mAmount) || TextUtils.isEmpty(mNote)){
-                    Toast.makeText(getApplicationContext(),"All Fields are  Required.. ",Toast.LENGTH_SHORT).show();
-                    return;
-                }
-
-                String date= new UtilitiesClass().getFormatedDate();
-
-                ShopingModle data = new ShopingModle(mType,ammint,mNote,date,id);
-                writeToDb(id,data,"Data Updated Successfully !! ");
-
-                dialog.dismiss();
-
-            }
-        });
-
-
-        btnDelete.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                mDatabase.child(id).removeValue();
-                readAllData(new UtilitiesClass().getFormatedDate());
-                //Total sum number
-                loadTotalAmount(new UtilitiesClass().getFormatedDate());
-                Toast.makeText(getApplicationContext(),"Data Deleted Successfully !! ",Toast.LENGTH_SHORT).show();
-                dialog.dismiss();
-
-            }
-        });
-
-
-
-    }
-
-    public void writeToDb(String id,ShopingModle data, String type)
-    {
-        mDatabase.child(id).setValue(data).addOnCompleteListener(new OnCompleteListener<Void>() {
-            @Override
-            public void onComplete(@NonNull Task<Void> task) {
-                if(task.isSuccessful())
-                {
-                    Toast.makeText(getApplicationContext(),type,Toast.LENGTH_SHORT).show();
-                    readAllData(new UtilitiesClass().getFormatedDate());
-                }
-                else {
-                    String error = task.getException().getMessage();
-                    Toast.makeText(getApplicationContext(),error,Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-    }
 }
