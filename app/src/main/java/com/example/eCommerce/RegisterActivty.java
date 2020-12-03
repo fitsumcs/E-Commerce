d7f34ebb342e18c6d73d9e670a6944fb9d16e297
Fitsum Alemu <fitsumcs@gmail.com>
2020-12-03 19:49:17 +0300

    Adding user  regestiration

--- app/src/main/java/com/example/eCommerce/RegisterActivty.java
+++ app/src/main/java/com/example/eCommerce/RegisterActivty.java
@@ -17,14 +17,18 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RegisterActivty extends AppCompatActivity {
 
-    private EditText email;
-    private EditText pass;
+
+    private EditText  ed_fName, ed_lName, ed_email, ed_phone,ed_pass,ed_pass2;
     private Button btnReg;
 
-    private FirebaseAuth mAuth;
+    private FirebaseAuth firebaseAuth;
+    private DatabaseReference usersDatabase;
 
     private ProgressDialog mDialog;
 
@@ -33,45 +37,107 @@ public class RegisterActivty extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_activty);
 
-        mAuth= FirebaseAuth.getInstance();
+        firebaseAuth= FirebaseAuth.getInstance();
+        usersDatabase = FirebaseDatabase.getInstance().getReference("Users");
 
         mDialog=new ProgressDialog(this);
 
-        email=findViewById(R.id.email_reg);
-        pass=findViewById(R.id.password_reg);
+        ed_fName = findViewById(R.id.first_reg);
+        ed_lName = findViewById(R.id.last_reg);
+        ed_email = findViewById(R.id.email_reg);
+        ed_phone = findViewById(R.id.phone_reg);
+        ed_pass = findViewById(R.id.password_reg);
+        ed_pass2 = findViewById(R.id.password_reg2);
+
+
 
         btnReg=findViewById(R.id.btn_reg);
+        btnReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String fName = ed_fName.getText().toString().trim();
+                String lName =  ed_lName.getText().toString().trim();
+                String uemail =  ed_email.getText().toString().trim();
+                String uphone = ed_phone.getText().toString().trim();
+                String upass = ed_pass.getText().toString().trim();
+                String upass2 = ed_pass2.getText().toString().trim();
+
+                if(TextUtils.isEmpty(fName) || TextUtils.isEmpty(lName) || TextUtils.isEmpty(uemail) || TextUtils.isEmpty(uphone) || TextUtils.isEmpty(upass) || TextUtils.isEmpty(upass2))
+                {
+                     Toast.makeText(RegisterActivty.this,"Please Fill All Fields!!",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                else if(upass.length() < 6)
+                {
+                    Toast.makeText(RegisterActivty.this,"Password Length Must be Greater than 6!!",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                else if(!upass.equalsIgnoreCase(upass2))
+                {
+                    Toast.makeText(RegisterActivty.this,"Password Does not Match!!",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+
+                registerUser(fName,lName,uemail, uphone,upass);
+            }
+        });
 
     }
 
 
 
-    public void registerUser(View view) {
-        String mEmail=email.getText().toString().trim();
-        String mPass=pass.getText().toString().trim();
+    private void registerUser(String userFirstName, String userLastName, String uemail,String userPhoneNumber,String upassword) {
 
-        if (TextUtils.isEmpty(mEmail) || TextUtils.isEmpty(mPass)){
-            email.setError("All Fields are  Required ..");
-            return;
-        }
-
-        mDialog.setMessage("Processing..");
+        mDialog.setMessage("Please Wait...");
         mDialog.show();
-        mAuth.createUserWithEmailAndPassword(mEmail,mPass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+
+        firebaseAuth.createUserWithEmailAndPassword(uemail,upassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
+
                 mDialog.dismiss();
-                if (task.isSuccessful()){
-                    startActivity(new Intent(getApplicationContext(),HomeActivity.class));
-                    Toast.makeText(getApplicationContext(),"Successfully Registered !!",Toast.LENGTH_SHORT).show();
 
-                }else {
-                    String error = task.getException().getMessage();
-                    Toast.makeText(getApplicationContext(),error,Toast.LENGTH_SHORT).show();
+                if(task.isSuccessful())
+                {
+
+                    //get user
+                    FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+                    String userId = firebaseUser.getUid();
+
+                    User user  = new User(userFirstName, userLastName,uemail, userPhoneNumber);
+
+                    //add data
+                    usersDatabase.child(userId).setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if(task.isSuccessful())
+                            {
+                                Toast.makeText(RegisterActivty.this,"Successfully Registered You can Login!!",Toast.LENGTH_SHORT).show();
+                                ed_fName.setText("");
+                                ed_lName.setText("");
+                                ed_email.setText("");
+                                ed_phone.setText("");
+                                ed_pass.setText("");
+                                ed_pass2.setText("");
+                                Intent intent = new Intent(RegisterActivty.this,HomeActivity.class);
+                                startActivity(intent);
+                            }
+                            else{
+                                Toast.makeText(RegisterActivty.this,"Some thing went Wrong Try again",Toast.LENGTH_SHORT).show();
+                            }
+
+
+                        }
+                    });
+
                 }
-
+                else{
+                    String errMesg = task.getException().getMessage();
+                    Toast.makeText(RegisterActivty.this,errMesg,Toast.LENGTH_SHORT).show();
+                }
             }
         });
-
     }
 }
